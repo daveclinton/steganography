002776244e91f1217d2a15ac5480603d63ac38b2
David Clinton <clinton@192.168.0.103>
2024-08-22 22:19:47 +0300

    feat: add econde message function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,63 @@
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(author, version, about, lang_about = None)]
+
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Encode {
+        input: PathBuf,
+        message: String,
+        output: PathBuf,
+    },
+    Decode {
+        input: PathBuf,
+    },
+    Remove {
+        input: PathBuf,
+        ouput: PathBuf,
+    },
+    List {
+        input: PathBuf,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Encode {
+            input,
+            message,
+            output,
+        } => match encode_message(&input, &message, &output) {
+            Ok(_) => println!("Message encoded succesfully"),
+            Err(e) => eprintln!("Error encoding message: {}", e),
+        },
+        Commands::Decode { input } => {
+            // Decode e=message from PNG file
+        }
+        Commands::Remove { input, ouput } => {
+            // Remove the message from the PNG file
+        }
+        Commands::List { input } => {
+            // List the chunks in the PNG file
+        }
+    }
+}
+
+use png::{BitDepth, ColorType, Encoder, HasParameters};
+
+fn encode_message(
+    input_path: &PathBuf,
+    message: &str,
+    output_path: &PathBuf,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut decoder = png::Decoder::new(File::open(input_path)?);
 }
